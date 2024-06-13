e0d0ac999566368cf5873c7f18fa3c43a1a0759e
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-13 19:34:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH", "")
+	API_ID = int(os.environ.get("API_ID", "12618934"))
+	API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "@ARAKAL_THERAVAD_MOVIES_V7_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001974433785"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1297128957"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Autofilterv7:Autofilterv7@cluster0.t5tqe4s.mongodb.net/")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002010307613")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002010307613")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
