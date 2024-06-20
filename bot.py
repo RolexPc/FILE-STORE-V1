c4d1ee7c0fc26de5bac29758c732b1475e648c37
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-20 11:33:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ async def start(bot: Client, cmd: Message):
         # Send image before welcome text
         await bot.send_photo(
             chat_id=cmd.from_user.id,
-            photo=f"https://telegra.ph/file/bb8d3fef33012c904cd8c.jpg"  # Replace with your image URL
+            photo=f"'https://telegra.ph/file/ee0cdd28fdd53d3df26c7.jpg https://telegra.ph/file/def62cc4e67aabc03d4c8.jpg https://telegra.ph/file/0c04c8e0b4a67d1716f53.jpg https://telegra.ph/file/eccd0bd3f18111afb9726.jpg https://telegra.ph/file/d6a99238948a8918e3e94.jpg https://telegra.ph/file/fcaf8e6dbadb19364ab54.jpg https://telegra.ph/file/6ca5d5dc24e029c4fe642.jpg https://telegra.ph/file/80a417b7cfc6dc262dcee.jpg https://telegra.ph/file/0426befe276a430a6cdf0.jpg https://telegra.ph/file/7d716533a70fad271a5b9.jpg')).split()"  # Replace with your image URL
         )
 
         await cmd.reply_text(
